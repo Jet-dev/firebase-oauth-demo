32531685a1471ae96075108bcfd9f4effc1bef73
Theo Berkhout <theoberkhout59@gmail.com>
2023-09-21 14:47:24 +0200

    :wrench: configure firebase

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import './style.css'
+import { initializeApp } from "firebase/app";
+import { environment } from '../environment.ts';
 
+initializeApp(environment.firebaseConfig);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
